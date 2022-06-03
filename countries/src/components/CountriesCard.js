317054c8830ea87ec5ebd5d6072d75a2cb6bd243
vajihemansoori <vajihe.mansoori76@gmail.com>
2022-06-03 14:53:59 +0430

    add filter

--- countries/src/components/CountriesCard.js
+++ countries/src/components/CountriesCard.js
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const CountriesCard = ({ countriesData }) => {
   const [search, setSearch] = useState("");
-  const searchText = (e) => {
-    setSearch(e.target.value);
-  };
-  console.log(countriesData.name.common);
-  let dataSearch = countriesData.filter((item) => {
-    return Object.keys(item).some((key) =>
-      item[key]
-        .toString()
-        .toLowerCase()
-        .includes(search.toString().toLowerCase())
-    );
-  });
+  const [filter, setFilter] = useState("");
+
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    setFilteredData(countriesData);
+
+    if (search || filter) {
+      const handleFilter = () => {
+        const data = countriesData
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(search.toLowerCase())
+          )
+          .filter((country) => country.region === filter);
+        setFilteredData(data);
+      };
+      handleFilter();
+    }
+  }, [countriesData, search, filter]);
+
   return (
     <>
       <div className="nav">
@@ -23,37 +31,45 @@ const CountriesCard = ({ countriesData }) => {
             type="text"
             placeholder="Search"
             value={search}
-            onChange={searchText.bind(this)}
+            onChange={(e) => setSearch(e.target.value)}
           />
         </div>
-        <div className="select-box"></div>
+        <div className="select-box">
+          <select onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Filter By Region</option>
+            <option value="Africa">Africa</option>
+            <option value="Americas">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
       </div>
       <div className="container">
-        {countriesData &&
-          dataSearch.map((data, index) => {
-            const { flags, name, population, region, capital } = data;
-            return (
-              <Link
-                key={index}
-                to={`/CountrieDetail/${name.common}`}
-                state={data}
-              >
-                <div className="card">
-                  <img src={flags.png} alt={name.common} />
-                  <h3>{name.common}</h3>
-                  <p>
-                    Population: <span>{population}</span>
-                  </p>
-                  <p>
-                    Region: <span>{region}</span>
-                  </p>
-                  <p>
-                    Capital: <span>{capital}</span>
-                  </p>
-                </div>
-              </Link>
-            );
-          })}
+        {filteredData.map((data, index) => {
+          const { flags, name, population, region, capital } = data;
+          return (
+            <Link
+              key={index}
+              to={`/CountrieDetail/${name.common}`}
+              state={data}
+            >
+              <div className="card">
+                <img src={flags.png} alt={name.common} />
+                <h3>{name.common}</h3>
+                <p>
+                  Population: <span>{population}</span>
+                </p>
+                <p>
+                  Region: <span>{region}</span>
+                </p>
+                <p>
+                  Capital: <span>{capital}</span>
+                </p>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </>
   );
